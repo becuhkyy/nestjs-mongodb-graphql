e859091619cb0025f165fd610c7a92167ea1f45f
Vesko Vasilev <payvesipal@gmail.com>
2024-08-23 19:00:02 +0300

    getLessons

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -8,6 +8,11 @@ import { CreateLessonInput } from './lesson.input';
 export class LessonResolver {
   constructor(private lessonService: LessonService) {}
 
+  @Query((returns) => [LessonType])
+  lessons() {
+    return this.lessonService.getLessons();
+  }
+
   @Query((returns) => LessonType)
   lesson(@Args('id') id: string) {
     return this.lessonService.getLesson(id);
