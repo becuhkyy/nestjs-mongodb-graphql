f5e002058ae21d4d7cc145ab4ebb2117893a8d0e
Vesko Vasilev <payvesipal@gmail.com>
2024-08-23 15:44:22 +0300

    setup graphql with apollo driver, create a type & resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
+      driver: ApolloDriver,
     }),
+    LessonModule,
   ],
 })
 export class AppModule {}
