8e0b56d6a296766e88d91529591e5c601524b2c5
Vesko Vasilev <payvesipal@gmail.com>
2024-08-24 11:15:18 +0300

    add method for assigning students to lesson

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -4,6 +4,7 @@ import { Lesson } from './lesson.entity';
 import { Repository } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 import { CreateLessonInput } from './lesson.input';
+import { AssingStudentsToLessonInput } from './assign-students.input';
 
 @Injectable()
 export class LessonService {
@@ -20,14 +21,27 @@ export class LessonService {
   }
 
   async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
-    const { name, startDate, endDate } = createLessonInput;
+    const { name, startDate, endDate, students } = createLessonInput;
 
     const lesson = this.lessonRepository.create({
       id: uuid(),
       name,
       startDate,
       endDate,
+      students,
     });
+
+    return this.lessonRepository.save(lesson);
+  }
+
+  async assingStudentsToLesson(
+    assignStudentsToLessonInput: AssingStudentsToLessonInput,
+  ): Promise<Lesson> {
+    const { lessonId: id, studentIds } = assignStudentsToLessonInput;
+
+    const lesson = await this.lessonRepository.findOneBy({ id });
+
+    lesson.students = [...lesson.students, ...studentIds];
 
     return this.lessonRepository.save(lesson);
   }
