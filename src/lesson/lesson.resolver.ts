e16847832121c50bbde6eaef9bcab607ea33a726
Vesko Vasilev <payvesipal@gmail.com>
2024-08-24 11:16:42 +0300

    import the student module, add a method for assigning students to lesson & a method for resolving studentIds to students

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,12 +1,24 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
 import { Lesson } from './lesson.entity';
 import { CreateLessonInput } from './lesson.input';
+import { AssingStudentsToLessonInput } from './assign-students.input';
+import { StudentService } from 'src/student/student.service';
 
 @Resolver(() => LessonType)
 export class LessonResolver {
-  constructor(private lessonService: LessonService) {}
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService,
+  ) {}
 
   @Query(() => [LessonType])
   lessons() {
@@ -23,5 +35,20 @@ export class LessonResolver {
     @Args('createLessonInput') createLessonInput: CreateLessonInput,
   ): Promise<Lesson> {
     return this.lessonService.createLesson(createLessonInput);
+  }
+
+  @Mutation(() => LessonType)
+  assignStudentsToLesson(
+    @Args('assingStudentsToLessonInput')
+    assignStudentsToLessonInput: AssingStudentsToLessonInput,
+  ) {
+    return this.lessonService.assingStudentsToLesson(
+      assignStudentsToLessonInput,
+    );
+  }
+
+  @ResolveField()
+  students(@Parent() lesson: Lesson) {
+    return this.studentService.getManyStudents(lesson.students);
   }
 }
